fd6bd5469dbb6f9436cf590178ae9272e92c6c07
Ignas Kavaliauskas <ignas.kavaliauskas@celeraone.com>
2019-10-08 13:01:18 +0200

    ROT13 functionality added
    Signed-off-by: Ignas Kavaliauskas <ignas.kavaliauskas@celeraone.com>

--- src/rot13.js
+++ src/rot13.js
@@ -1,11 +1,27 @@
 
+function rot13(input) {
+    var re = new RegExp("[a-z]", "i");
+    var min = 'A'.charCodeAt(0);
+    var max = 'Z'.charCodeAt(0);
+    var factor = 13;
+    var result = "";
+    input = input.toUpperCase();
+
+    for (var i = 0; i < input.length; i++) {
+        result += (re.test(input[i]) ?
+            String.fromCharCode((input.charCodeAt(i) - min + factor) % (max - min + 1) + min) : input[i]);
+    }
+
+    return result.toLowerCase();
+}
+
 function rot13_encode(input) {
-    return "this is the encoded value of " + input
+    return rot13(input);
 }
 
 function rot13_decode(input) {
-    return "this is the decoded value of " + input
+    return rot13(input);
 }
 
 
-export {rot13_encode, rot13_decode};
+export { rot13_encode, rot13_decode };
